70806d3155e5683dc676541ab201489293b9421b
Jarmo <jarmo.niemi@artific.ai>
2025-07-23 15:49:53 +0300

    Get tools and robot from their own modules in 'make_tea.py' script

--- robot_ai/tea_maker/make_tea.py
+++ robot_ai/tea_maker/make_tea.py
@@ -1,12 +1,9 @@
-import os
 import logging
 import sys
-from typing import List
 
 from google import genai
 from lerobot.record import RecordConfig
 from lerobot.configs import parser
-from lerobot.common.robots.utils import make_robot_from_config
 
 from robot_ai.constants import GEMINI_API_KEY, TOOL_EXECUTION_VIDEO_DIR_PATH
 from robot_ai.lerobot_utils.record import (
@@ -15,52 +12,23 @@ from robot_ai.lerobot_utils.record import (
     HOME_POSITION,
     RECORDED_VIDEO_PATH,
 )
+from robot_ai.lerobot_utils.robot import ROBOT
 from robot_ai.tea_maker.instructions import get_task_instruction, TASK_MAKE_TEA
 from robot_ai.tasks import (
-    Tool,
     get_task_plan,
     analyze_if_tool_was_used_successfully,
     ask_for_help,
 )
+from robot_ai.tea_maker.tools import get_tea_making_tools
 
 
 logger = logging.getLogger()
 
 
-def get_tea_making_tools() -> List[Tool]:
-    return [
-        Tool(
-            name="pick_teabag_drop_in_cup",
-            model_path=os.environ.get("PICK_TEABAG_DROP_IN_CUP"),
-            execution_time_in_seconds=40,
-            success_validation_question="Was a teabag added into the teacup?",
-        ),
-        Tool(
-            name="remove_teabag_from_cup",
-            model_path=os.environ.get("REMOVE_TEABAG_FROM_CUP"),
-            execution_time_in_seconds=40,
-            success_validation_question="Was the teabag removed from the teacup?",
-        ),
-        Tool(
-            name="sugar_cube_in_cup",
-            model_path=os.environ.get("SUGAR_CUBE_IN_CUP"),
-            execution_time_in_seconds=60,
-            success_validation_question="Was a sugar cube added into the teacup?",
-        ),
-        Tool(
-            name="stir_spoon",
-            model_path=os.environ.get("STIR_SPOON"),
-            execution_time_in_seconds=40,
-            success_validation_question="Was the teacup stirred with the spoon that was inside the teacup?",
-        ),
-    ]
-
-
 @parser.wrap()
 def main(cfg: RecordConfig):
+    ROBOT.connect()
     genai_client = genai.Client(api_key=GEMINI_API_KEY)
-    robot = make_robot_from_config(cfg.robot)
-    robot.connect()
 
     tea_making_tools = get_tea_making_tools()
     task_instruction = get_task_instruction(task=TASK_MAKE_TEA)
@@ -88,10 +56,10 @@ def main(cfg: RecordConfig):
             cfg.dataset.root = tool_execution_video_dir_path
 
             logger.info(f"Running tool: {tool.name} (try {tool_execution_tries + 1})")
-            record(cfg=cfg, robot=robot)
+            record(cfg=cfg, robot=ROBOT)
 
             logger.info("Return to home position")
-            return_to_home_position(robot=robot, home_position=HOME_POSITION)
+            return_to_home_position(robot=ROBOT, home_position=HOME_POSITION)
 
             logger.info("Analyzing if the tool was used successfully...")
             is_tool_usage_success = analyze_if_tool_was_used_successfully(
@@ -111,7 +79,7 @@ def main(cfg: RecordConfig):
                 )
                 is_tool_usage_success = True
 
-    robot.disconnect()
+    ROBOT.disconnect()
 
 
 if __name__ == "__main__":
